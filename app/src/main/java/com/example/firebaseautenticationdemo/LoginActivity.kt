07af2e6a599d439707eea9207e7abb727ef5425c
SaurabhS120 <saurabh.sonar120@gmail.com>
2022-07-13 16:16:51 +0530

    email login with built in

--- app/src/main/java/com/example/firebaseautenticationdemo/LoginActivity.kt
+++ app/src/main/java/com/example/firebaseautenticationdemo/LoginActivity.kt
@@ -8,6 +8,11 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.app.TaskStackBuilder
 import com.example.firebaseautenticationdemo.databinding.ActivityLoginBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.ActionCodeSettings
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding : ActivityLoginBinding
@@ -45,6 +50,10 @@ class LoginActivity : AppCompatActivity() {
             goToCreateAccountPage()
         }
 
+        binding.createAccountWithOtherMethodsButton.setOnClickListener {
+            signIn()
+        }
+
     }
 
     fun goToCreateAccountPage(){
@@ -55,5 +64,47 @@ class LoginActivity : AppCompatActivity() {
     fun goToLoggedInActivity(){
         val intent = Intent(this,LoggedInActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
+    }
+
+    // See: https://developer.android.com/training/basics/intents/result
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+    fun signIn(){
+        val actionCodeSettings = ActionCodeSettings.newBuilder()
+            .setAndroidPackageName( /* yourPackageName= */
+                "com.example.firebaseautenticationdemo",  /* installIfNotAvailable= */
+                true,  /* minimumVersion= */
+                null)
+            .setHandleCodeInApp(true) // This must be set to true
+            .setUrl("https://google.com") // This URL needs to be whitelisted
+            .build()
+
+        val providers = listOf(
+            AuthUI.IdpConfig.EmailBuilder()
+                .setActionCodeSettings(actionCodeSettings)
+                .build()
+        )
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            Utils.showToast(this,"Firebase login success")
+            goToLoggedInActivity()
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+            Utils.showToast(this,"Firebase login failed")
+        }
     }
 }
